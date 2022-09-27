6bd942dba1feb585bc04a1b1de5182de0cc02f6a
David Eriksson <david.eriksson@lundkroken.se>
2022-09-27 13:52:06 +0200

    Stavfel på rad 15

--- helper_scripts/nas_start.sh
+++ helper_scripts/nas_start.sh
@@ -12,7 +12,7 @@ login=$(gpg --decrypt /etc/backups/credentials.gpg)
 
 ether-wake -i eth0 A0:21:B7:C1:D1:8E
 
-timeout 60 bash -c -- 'while ! ping -c 1 -n -w 1 $nas &> /dev/null; do sleep 1;done;'
+timeout 60 bash -c -- "while ! ping -c 1 -n -w 1 $nas &> /dev/null; do sleep 1;done;"
 nas_exit=$?
 if [ $nas_exit -ne 0 ]; then
     echo "ERROR: NAS did not come online!"
